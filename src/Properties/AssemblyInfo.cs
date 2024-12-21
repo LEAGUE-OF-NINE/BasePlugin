cce5fe3b3cd84302a3217d7b1ab0010d133efde3
pufferffish <me@windtfw.com>
2024-12-21 03:39:16 +0000

    fix build

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,17 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using BaseMod;
+using static BepInEx.EnableResize;
 
-[assembly: AssemblyTitle(main.NAME)]
-[assembly: AssemblyCompany(main.AUTHOR)]
-[assembly: AssemblyProduct(main.NAME)]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-
-
-[assembly: ComVisible(false)]
-
-
-[assembly: Guid("b8f92b29-0e76-4151-b668-3b545ab0de25")]
-
-[assembly: AssemblyVersion(main.VERSION)]
-[assembly: AssemblyFileVersion(main.VERSION)]
+[assembly: AssemblyTitle(GUID)]
+[assembly: AssemblyDescription(PluginName)]
+[assembly: AssemblyProduct(GUID)]
+[assembly: AssemblyVersion(PluginVersion)]
+[assembly: AssemblyFileVersion(PluginVersion)]
